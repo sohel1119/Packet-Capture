2ec6f75037aad9a9e6705785e8c1b9b9405d1f13
sohel1119 <94044610+sohel1119@users.noreply.github.com>
2022-11-15 14:00:52 +0530

    Update pcapcapture.py

--- pcapcapture.py
+++ pcapcapture.py
@@ -2,4 +2,4 @@ import scapy.all as scapy
 t = scapy.AsyncSniffer(iface="PutInterfaceName",filter="udp port 2123")
 t.start()  # before sending packets
 Pkts=t.stop()  #after sending packets
-scapy.wrpcap("/tmp/test1.pcap" ,Pkts)
+scapy.wrpcap("/tmp/test1.pcap" ,Pkts) #path
